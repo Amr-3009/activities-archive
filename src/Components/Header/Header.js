38bfeb4b70abce7adf49008295e2fd68d3e05009
Amr-3009 <amrosama1k@gmail.com>
2024-10-13 12:51:37 +0300

    Refactor component files and add routing support
    - Refactor Footer.js, Titlebar.js, Header.js to use CSS modules
    - Add Footer.css, Titlebar.css, Header.css for styling
    - Import react-router-dom package for routing support
    - Update package.json to include react-router-dom dependency
    - Remove unused files: setupTests.js, App.test.js, reportWebVitals.js
    - Update index.css to import Google Fonts
    - Update App.js to import and render Header component
    - Update index.js to use ReactDOM.createRoot
    - Update App.css to reset padding, margin, and box-sizing
    - Update public/index.html to change page title to "Activities Archive"

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <p>
+        Tarbyh Namouthajiyah Schools in Alnarjis & Qurtoba are now open{" "}
+        <a href="https://ncle-sms.com/Admission" target="blank">
+          Register Now
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/Components/Titlebar/Titlebar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Titlebar.css";
+
+const Titlebar = () => {
+  return <div>Titlebar</div>;
+};
+
+export default Titlebar;
